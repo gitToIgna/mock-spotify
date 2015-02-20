296aad8bfc3864c950b79d65206722f8c1165b3d
htmmc <hitomi.mzgk@gmail.com>
2015-02-20 00:17:25 -0500

    added positioning features for tracks

--- js/app.js
+++ js/app.js
@@ -55,17 +55,20 @@ $('#search_artist').on('click', function(event){
 		// display
 		myAlbums.forEach(function (album) {
 			$('.albumList').append(`
-				<div class="album_data_list col-lg-6 alight="center">
+				<div class="album_data_list col-lg-6" align="center">
 				 	<li data-album="${album.id}">
+				 		<div class="album_upper_container">
 				        <img src="${album.cover}" class="album_image">
 				        <h2>"${album.title}"</h2>
 				        <button class="seetracks">See Tracks</button>
+				        </div>
 				        <ul class="tracklist"></ul>
-				    </li>
+				     </li>
 			    </div>`
 			);
-			//don't forget to use ` for multi-line string
+		//don't forget to use ` for multi-line string
 		})
+		var trackDataList = false;
 
 		$(".seetracks").on("click", function(event){
 			event.preventDefault();
@@ -73,6 +76,7 @@ $('#search_artist').on('click', function(event){
 			var albumEl = button.closest('li');
 			var id = albumEl.data("album");
 			var trackUrl = "https://api.spotify.com/v1/albums/" + id;
+			if (trackDataList == false) {
 			$.ajax(trackUrl)
 				.done(function (response) {
 					var tracks = response.tracks.items.map(function (data) {
@@ -86,21 +90,25 @@ $('#search_artist').on('click', function(event){
 				tracks.forEach(function (track){
 				//display
 					$(event.target.parentElement).find(".tracklist").append(`
-					 	<li class="track">
-					        <h3>${track.trackNumber}: ${track.title}</h3>
+					 	<li class="track clearfix">
+					        <h3 class="track_title clearfix">${track.trackNumber}: ${track.title}</h3>
 					        <audio src="${track.preview}" controls></audio>
 					    </li>`
 					);
 				});
-			});
-				
+			});		
+			trackDataList = true;
+			
+			} else {
+				$(event.target.children).find("ul.tracklist").hide()
+				trackDataList = false;
+			}
+
 		});
+
+
 	})
 	.error(function (err) {
 		console.log(err);
 	})
 })
-
-
-
-
